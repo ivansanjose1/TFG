534c944396909eca1dddcf76bc52dacefbeb645a
Joey-Yuki <116670104+Joey-Yuki@users.noreply.github.com>
2023-05-29 09:14:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SerializableYugi
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            try
+            {
+                Application.Run(new Form1());
+            }catch (Exception ex) { }
+        }
+    }
+}
